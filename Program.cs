9b181c12ab81395dd279dbf4cf6180aa930e662a
Khalitov62 <tu7m5rk73code.edu.az>
2022-04-24 09:30:48 +0400

    added

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Home
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string subject = Console.ReadLine();
+            Students students = new Students("Khalitov Islam", subject);
+            Students students1 = new Students("Khalitov Islam", subject);
+            Students students2 = new Students("Khalitov Islam", subject);
+            Console.WriteLine(students.GetName(subject) + students.Count);
+            Console.WriteLine(students1.GetName(subject) + students1.Count);
+        }
+    }
+}
---
+++ Students.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Home
+{
+    internal class Students
+    {
+        public static int _id { get; set; }
+        public int Id { get; set; }
+        public string FullName { get; set; }
+        public string Subject { get; set; }
+        private static int _count = 1000;
+        public int Count;
+
+
+        public Students(string fullName, string subject)
+        {
+            _count++;
+            Count = _count;
+            _id++;
+            Id = _id;
+            FullName = fullName;
+            Subject = subject;
+        }
+
+        public string GetName(string subject)
+        {
+            return char.ToUpper(subject[0]) + char.ToUpper(subject[1]).ToString();
+        }
+        
+    }
+}
